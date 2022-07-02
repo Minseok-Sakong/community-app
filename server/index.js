876aeb4bf63c4b4bc3d560917aea974f1bd68711
minseoksakong <msakong@bu.edu>
2022-07-02 18:21:07 -0400

    connected with mongodb

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const path = require("path");
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 5000;
-
+//mongodb+srv://msakong40:Ec88Vrj759gJDH@cluster0.y4dwt.mongodb.net/?retryWrites=true&w=majority
 app.use(express.static(path.join(__dirname, '../client/build')))
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  mongoose.connect(
+    'mongodb+srv://msakong40:Ec88Vrj759gJDH@cluster0.y4dwt.mongodb.net/?retryWrites=true&w=majority'
+    ).then(() => {
+      console.log(`Example app listening on port ${port}`)
+      console.log(`Connecting mongodb`)
+    }).catch((err)=> {
+      console.log(`${err}`)
+    })
 })
 
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  })
+
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'))
   })
