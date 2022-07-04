5ea8abbf3f6b95e119ede0e534011bbdf0cb5a31
minseoksakong <msakong@bu.edu>
2022-07-04 18:39:27 -0400

    implemented logout feature

--- client/src/Component/Heading.js
+++ client/src/Component/Heading.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { useSelector } from "react-redux";
+import firebase from "../firebase.js";
 
 const Heading = () => {
+  const user = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const LogoutHandler = () => {
+    firebase.auth().signOut();
+    navigate("/");
+  };
   return (
     <Navbar bg="dark" expand="lg" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Community-App</Navbar.Brand>
+        <Navbar.Brand href="/">Minseok's Community App</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -34,18 +42,27 @@ const Heading = () => {
             >
               upload
             </Link>
-
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Collapse className="justify-content-end">
+          {user.accessToken === "" ? (
             <Link
               to="/login"
               style={{
                 color: "white",
                 textDecoration: "none",
-                marginRight: "1rem",
               }}
             >
               login
             </Link>
-          </Nav>
+          ) : (
+            <Navbar.Text
+              style={{ color: "white", cursor: "pointer" }}
+              onClick={() => LogoutHandler()}
+            >
+              Logout
+            </Navbar.Text>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
